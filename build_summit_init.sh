40014badf10301cce8178b2875c6e7fb28c0cf0e
Mathialakan Thavappiragasam <mathialakan@gmail.com>
2022-06-01 08:17:37 -0400

    Add build script for summit

---
+++ build_summit_init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Load modules
+#--------------------- 
+ module load python cuda gcc cmake
+
+#Create environment 
+#--------------------- 
+#conda create -n galgo 
+
+#Activate environment
+ source ~/.bashrc
+ conda activate galgo 
+ 
+#Install conan 
+#--------------------- 
+ conda install -c conda-forge conan 
+
+#Setup conan profile 
+#--------------------- 
+ conan profile new default --detect 
+ conan profile update settings.compiler.libcxx=libstdc++11 default 
+ conan profile update settings.compiler.version=9.1 default
+ conan profile update env.CC=$(which gcc) default 
+ conan profile update env.CXX=$(which g++) default 
+
+#Install dependencies  
+#--------------------- 
+ conan create conan/waf-generator user/stable 
+ conda --version 
+ conan create conan/trng 4.22@user/stable 
+ conan install . --build fmt
+ conan install . --build spdlog 
+ conan install . --build nvidia-cub
+
+#Configure and build
+#--------------------- 
+ ./waf configure --enable-cuda build_relese
